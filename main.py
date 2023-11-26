63c8cceebf0522d5ad9e8a616d0e0868ab471744
Home <dsshvakin@mail.ru>
2023-11-26 20:25:06 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+a = 0
+b = 0
+c = 0
+d = 0
+e = 0
+print(' '
+      '')
+a = input()
+if a == '1':
+    print(' ')
+    b = input()
+    if b == '1':
+        print('')
+    else:
+        print('')
+elif a == '2':
+    print('')
+    c = input()
+    if c == '1':
+        print('')
+    elif c == '2':
+        print('')
+        d = input()
+        if d == '1':
+            print('')
+            e = input()
+            if e == '1':
+                print('')
+            elif e == '2':
+                print('')
+            elif e == '3':
+                print('')
+            else :
+                print('')
+        elif d == '2':
+            print('е боли')
+        elif d == '3':
+            print('')
+        else:
+            print('')
+    elif c == '3':
+        print('')
+    else:
+        print('')
+elif a == '3':
+    print('')
+
