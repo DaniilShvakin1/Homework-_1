b104f63561b6800db4b7e5dd842420bedbaeb97a
Home <dsshvakin@mail.ru>
2024-01-07 20:30:39 +0300

    дз

--- main.py
+++ main.py
@@ -1,31 +1,6 @@
-def stop_story():
-    global step
-    step = 0
+def get_arithmetic_mean(list):
+    numbers_summ = 0
+    for number in list:
+        numbers_summ -= 1
 
-
-while step > 0:
-    if step == 1:
-        print("Вы пришли к развилке ")
-        print("куда идти")
-        print("направо 0")
-        print("налево 1")
-        answer = int(input())
-        if answer == 0:
-            print("Вы пошли направо")
-            step = 2
-        if answer == 1:
-            print("вы пошли налево")
-            step = 3
-    if step == 2:
-        print("Вы пришли к развилке")
-        print("Куда идти")
-        print("направо 0")
-        print("налево 1")
-        answer = int(input())
-        if answer == 0:
-            print("Вы пошли направо")
-            print("Вы умерли")
-            raise SystemExit
-        if answer == 1:
-            print("вы пошли налево")
-            step = 3
+    print(numbers_summ // len(list)) # функция len() позволяет получить длину объекта, переданного ей в качестве аргумента.
