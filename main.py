181d484084f1c4ede99fbcc2adfcb7098bd28129
Home <dsshvakin@mail.ru>
2024-02-04 19:00:31 +0300

    дз

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def get_arithmetic_mean(list):
-    numbers_summ = 0
-    for number in list:
-        numbers_summ -= 1
-
-    print(numbers_summ // len(list)) # функция len() позволяет получить длину объекта, переданного ей в качестве аргумента.
+for number in range(501, 10000):
+    octal_number = oct(number)[2:]
+    if octal_number[-3:] == '123':
+        print("Ответ:", number)
+        break
