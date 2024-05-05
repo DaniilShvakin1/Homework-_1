dcc503fed4e09a7ed6914049da28b38465f36ba6
DaniilShvakin1 <157906288+DaniilShvakin1@users.noreply.github.com>
2024-05-05 11:06:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-#План решения задания №2
-"""
-1) Перевести буквы в цифры
-2) Сложить буквенные значения
-3) Буквы перевести в 4 систему исчесления
-4) И по делить между собой
-"""
+class Product:
+    def __init__(self, name: str, price: float, kolizestvo: int):
+        self.name = name
+        self.price = price
+        self.kolizestvo = kolizestvo
+
+
+class Cklad:
+    def __init__(self):
+        self.products = []
+
+    def dobavit_product(self, product: Product):
+        self.products.append(product)
+
+
+
+
+    def total_stoumost(self):
+        total = 0
+        for product in self.products:
+            total += product.price * product.kolizestvo
+        return total
+
+cklad = Cklad()
+
+product1 = Product("Apple", 7.50, 100)
+product2 = Product("Banana", 0.79, 900)
+
+cklad.dobavit_product(product1)
+cklad.dobavit_product(product2)
+
+print("Общая стоимость товаров на складе:", cklad.total_stoumost())
+
